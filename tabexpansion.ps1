62126e6efa6892f6fae8427812100355224dcf40
John Weldon <johnweldon4@gmail.com>
2012-03-22 15:48:55 -0700

    bug tweak

--- tabexpansion.ps1
+++ tabexpansion.ps1
@@ -1,5 +1,14 @@
 
 copy function:\TabExpansion function:\DefaultTabExpansion
+
+function optionally-quote {
+    param($script:str)
+    if($script:str.contains(" ") -or $script:str.startswith("{") -or [char]::isdigit($script:str.substring(0,1))) {
+        $script:str = ("'{0}'" -f $script:str);
+    }
+    return $script:str;
+}
+
 function TabExpansion {
 	param([string]$line, [string]$lastword)
 	if($line.tolower().trim().startswith("g:") -or $line.tolower().trim().startswith(":")) {
@@ -8,9 +17,9 @@ function TabExpansion {
 		    $script:q = $script:v.Project
 		    if($script:q.contains(" ")) { $script:q = ("'{0}'" -f $script:q) }
 			if(-not($lastword.endswith(":g ")) -or -not($lastword.endswith(": "))) {
-				if($script:v.Project.tolower().startswith($lastword.tolower())) { $script:q }
+				if($script:v.Project.tolower().startswith($lastword.tolower())) { (optionally-quote $script:v.Project) }
 			} else {
-				$script:q
+				(optionally-quote $script:v.Project)
 			}
 		}
 	} else {
