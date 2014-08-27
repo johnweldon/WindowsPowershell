0d4807592a84d0468d166e424d314dba26070a5d
John Weldon <johnweldon4@gmail.com>
2014-08-27 13:49:23 -0700

    dropbox

--- dropbox_profile.ps1
+++ dropbox_profile.ps1
@@ -9,24 +9,9 @@ $env:profile = (join-path $env:profile_dir "profile.ps1")
 $env:HOST = $env:COMPUTERNAME
 #$env:TERM = "msys" # http://stackoverflow.com/a/10820885/102371
 
-if(gcm -ErrorAction SilentlyContinue GetDropboxPath) {
-	$env:dropboxpath = (getdropboxpath)
-    # tcc
-	$env:PATH += (";{0}" -f (join-path $env:dropboxpath "tcc"))
-    # GO
-	if($false){
-        $env:GOROOT = (join-path (join-path $env:dropboxpath "gowin386_release.r60.3") "go")
-        $env:GOARCH = "386"
-    }
-    if($true){
-        $env:GOROOT = (join-path (join-path $env:dropboxpath "gowinamd64.1") "go")
-        $env:GOARCH = "amd64"
-    }
-    $env:GOPATH = (join-path (join-path $env:dropboxpath "source") "go_packages")
-    $env:GOBIN = (join-path $env:GOROOT "bin")
-    $env:PATH += (";{0}" -f $env:GOBIN)
+if(-not($env:dropboxpath) -and (gcm -ErrorAction SilentlyContinue GetDropboxPath)) {
+    $env:dropboxpath = (getdropboxpath)
 }
-
 
 
 ## source the following list of files
@@ -44,6 +29,12 @@ $sources =
 $sources | %{ join-path $env:profile_dir $_ } | ?{ test-path $_ } | %{ write-debug "loading $_";  . $_ }
 rm variable:\sources
 
+function rdp-yzma { mstsc /v:"yzma.jw4.us:23389" /f /multimon }
+function rdp-rack { mstsc /v:"portal.valuevision.com" /f /multimon }
+set-alias -name rpy -value rdp-yzma
+set-alias -name rpr -value rdp-rack
+
 write-debug "clean-path"
 clean-path
+set-location ~
 write-debug "DONE loading profile"
