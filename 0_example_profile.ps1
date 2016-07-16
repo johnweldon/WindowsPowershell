3a4abedb302655e631ae26898d0e48149540c423
John Weldon <John Weldon>
2016-07-16 10:42:37 -0700

    Add powershell Modules, update default config

--- 0_example_profile.ps1
+++ 0_example_profile.ps1
@@ -1,26 +1,59 @@
-$script:gdp = "GetDropboxPath.exe"
-if(get-command $script:gdp -ErrorAction SilentlyContinue) {
-    $env:DROPBOXPATH = (&$script:gdp)
+if ($env:vsconsoleoutput) { return }
 
-    # TODO: Uncomment if HOME doesn't co-incide with ~
-    # $script:fs = get-psprovider filesystem
-    # $script:fs.Home = $env:HOME
+$DebugPreference = "SilentlyContinue"
+$VerbosePreference = "SilentlyContinue"
 
-    $script:ppth = (Join-Path $env:DROPBOXPATH "WindowsPowershell/dropbox_profile.ps1")
-    $script:bpth = (Join-Path $env:DROPBOXPATH "bin")
+if (-not(test-path env:\GOPATH))        { $env:GOPATH = (resolve-path "C:\build\go") }
+if (-not(test-path env:\DROPBOXPATH))   { $env:DROPBOXPATH = (resolve-path "C:\build\dropbox\Dropbox") }
 
-    if(test-path $script:bpth) {
-        $env:PATH += ";" + $script:bpth
-    } else {
-        write-warning ("Path '{0}' doesn't exist" -f $script:bpth)
-    }
-    if(test-path $script:ppth) {
-        write-host "Loading..."
-        $env:profile_dir = split-path $script:ppth
-        . $script:ppth
-    } else {
-        write-warning ("Path '{0}' doesn't exist" -f $script:ppth)
-    }
+$env:PATH += ";" + (resolve-path (join-path $env:GOPATH "bin"))
+$env:PATH += ";" + (get-item "Env:ProgramFiles(x86)").Value + "\Git\bin"
+
+$script:fs = get-psprovider filesystem
+$script:fs.Home = $env:USERPROFILE
+
+$script:ppth = (resolve-path (join-path $env:DROPBOXPATH "WindowsPowershell/dropbox_profile.ps1"))
+$script:bpth = (resolve-path (join-path $env:DROPBOXPATH "bin"))
+$script:mpth = (resolve-path (join-path $env:DROPBOXPATH "WindowsPowershell/Modules"))
+
+if(test-path $script:bpth) {
+    $env:PATH += ";" + $script:bpth
 } else {
-	write-warning "GetDropboxPath not found"
+    write-warning ("Path '{0}' doesn't exist" -f $script:bpth)
 }
+if(test-path $script:ppth) {
+    write-host "Loading..."
+    $env:profile_dir = split-path $script:ppth
+    . $script:ppth
+} else {
+    write-warning ("Path '{0}' doesn't exist" -f $script:ppth)
+}
+if(test-path $script:mpth) {
+    $env:PSModulePath += ';'+$script:mpth
+} else {
+    write-warning ("Path '{0}' doesn't exist" -f $script:mpth)
+}
+
+
+
+function global:prompt { 
+    $realLASTEXITCODE = $LASTEXITCODE
+    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+    write-host($pwd.ProviderPath) -nonewline -foreground "green"
+    write-vcsstatus
+
+    $Host.UI.RawUI.WindowTitle = [string]("{0:yyyy-MM-dd HH:mm:ss} -- {1}" -f (get-date),(pwd).Path);
+    $local:p = " $ "
+    if(is-admin) {
+        $Host.UI.RawUI.WindowTitle = [string]("(ADMIN) {0:yyyy-MM-dd HH:mm:ss} -- {1}" -f (get-date),(pwd).Path);
+        write-host " #" -nonewline -foreground "red"
+    } else {
+        write-host " $" -nonewline -foreground "green"
+    }
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    return "> "
+}
+
+Import-Module PsGet
+Import-Module PsUrl
+Import-Module posh-git
